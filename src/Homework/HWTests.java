c385cd4b18ac40b5aee712deb76e30d8b185718d
michael1735 <25171080@qq.com>
2022-10-12 23:40:55 +0800

    pascal's triangle(yang hui san jiao) output code type1, using arr[10][10], but only assign and use its left part

--- src/Homework/HWTests.java
+++ src/Homework/HWTests.java
@@ -6,6 +6,7 @@ public class HWTests {
     // 2D arr exercises
     public static void main(String[] args) {
         // 1. 公司年销售额总和
+        /*
         Scanner input = new Scanner(System.in);
         int result = 0;
         int[][] myArr = new int[4][3];
@@ -23,5 +24,19 @@ public class HWTests {
         }
 
         System.out.println("年度销售总额: " + result);
+         */
+        int[][] arr = new int[10][10];
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j <= i; j++){
+                if (j == 0 || j == i){
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
