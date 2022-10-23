82436ffec6c504ed4eb2011ec58522a01e5f15fc
michael1735 <25171080@qq.com>
2022-10-23 22:01:30 +0800

    2022/10/23 22:01

--- src/Homework/ACSLIntermediate.java
+++ src/Homework/ACSLIntermediate.java
@@ -12,29 +12,8 @@ public class ACSLIntermediate {
          */
         Scanner input  = new Scanner(System.in);
         String[] inputStr = new String[5];
-        // String[] inputNumber = new String[5];
-        // String[] inputLength = new String[5];
-        long[][] inputContents = new long[5][2];
-        for (int i = 0; i < 5; i++){
-            inputStr[i] = input.nextLine();
-        }
-        for (int i = 0; i < 5; i++){
-            String[] temp;
-            temp = inputStr[i].split(" ");
-            for (int j = 0; j < 2; j++) {
-                // Exception in thread "main" java.lang.NumberFormatException: For input string: "54981230845791"
-                //	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
-                //	at java.base/java.lang.Integer.parseInt(Integer.java:668)
-                //	at java.base/java.lang.Integer.parseInt(Integer.java:784)
-                //	at Homework.ACSLIntermediate.main(ACSLIntermediate.java:25)
-                inputContents[i][j] = parseLong(temp[j].trim()); // <-- 这一行,我觉得我已经把空格去了
-            }
-        }
-        for (long[] i : inputContents){
-            for (long j : i){
-                System.out.println(j);
-            }
-        }
+        // 不能先转换成long, 因为这数字会overflow, 所以直接截取字符会好一些
+
     }
     /*
     public int calculateAns(String[] inputNumber, String[] inputLength){
