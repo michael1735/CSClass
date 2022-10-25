0ec2f2aa5b3d4194812a69a07b441c96f99b226a
michael1735 <25171080@qq.com>
2022-10-25 21:27:01 +0800

    2022/10/25 21:26

--- src/Notes/MethodsDemo.java
+++ src/Notes/MethodsDemo.java
@@ -1,5 +1,33 @@
 package Notes;
 
-public class MethodsDemo {
+import java.io.*;
 
+public class MethodsDemo {
+    public static String[] hireTime = new String[10];
+    public static int[] duration = new int[10];
+    public static double[] costs = new double[10];
+    public static double dailyTakings = 0;
+
+    public static void main(String[] args) {
+        try{
+            RecordHire(2, "12:00", 3, 12.0);
+        } catch (IOException e){
+            System.out.println("I/O error occurred when writing to HireLog.txt");
+        }
+    }
+    public static void RecordHire(int boatNum, String hireStartTime, int durationInMinutes, double cost) throws IOException {
+        final String COMMA = ",";
+
+        hireTime[boatNum] = hireStartTime;
+        duration[boatNum] = durationInMinutes;
+        costs[boatNum] = cost;
+
+        dailyTakings = dailyTakings + cost;
+
+        File file = new File("D:\\CS±Ê¼Ç\\HireLog.txt");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file, true)));
+        pw.write(boatNum + COMMA + hireStartTime + COMMA + durationInMinutes + COMMA + cost);
+        pw.flush();
+        pw.close();
+    }
 }
