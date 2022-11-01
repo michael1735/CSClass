aa602b2e36ca029bd60bad3dd61f56c485449de7
michael1735 <25171080@qq.com>
2022-11-01 16:00:16 +0800

    LinearSearch.java

---
+++ src/Notes/LinearSearch.java
@@ -0,0 +1,29 @@
+package Notes;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a value to search: ");
+        int searchValue = input.nextInt();
+        int[] myList = {2, 3, 4, 45, 34, 556};
+        int size = myList.length;
+        boolean found = false;
+        int i = 0;
+
+        while (i < size && found == false) {
+            if (myList[i] == searchValue) {
+                found = true;
+            } else {
+                i++;
+            }
+        }
+
+        if (found == true) {
+            System.out.println("Value found at index " + i);
+        } else {
+            System.out.println("Value not found");
+        }
+    }
+}
