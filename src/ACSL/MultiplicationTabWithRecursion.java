8005394e603b461c023f85bf3f4ce5fb5714dece
michael1735 <25171080@qq.com>
2022-11-18 18:36:57 +0800

    MultiplicationTabWithRecursion.java for using recursion to output the multiplication table for decimal numbers

--- src/ACSL/MultiplicationTabWithRecursion.java
+++ src/ACSL/MultiplicationTabWithRecursion.java
@@ -1,4 +1,20 @@
 package ACSL;
 
 public class MultiplicationTabWithRecursion {
+
+    public static void main(String[] args) {
+        printTab(9);
+    }
+
+    private static void printTab(int ix) {
+        if (ix == 1) {
+            System.out.println("1x1=1");
+            printTab(ix + 1);
+        }
+
+        for (int i = 1; i <= ix; i++) {
+            System.out.print(i + "x" + ix + "=" + i*ix + "\t");
+        }
+        System.out.println();
+    }
 }
