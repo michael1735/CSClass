4a1f1b3c7bad9e57a53311bd4c39e981f5ee33c2
michael1735 <25171080@qq.com>
2022-11-20 20:38:44 +0800

    interpolationSort(插值搜索)

--- src/Notes/BinarySearchDemo.java
+++ src/Notes/BinarySearchDemo.java
@@ -21,8 +21,30 @@ public class BinarySearchDemo {
        return -1;
     }
 
+    /**
+     * 插值查找, 是在二分查找的基础上, 改变二分查找的区间缩减策略, 根据搜索的值来确定区间缩减幅度, 使其不再是固定的1/2
+     */
+    public static int interpolationSearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        // 终止条件
+        while (left < right) {
+            // 中间位置计算
+            int mid = left + ((key - arr[left]) / (arr[right] - arr[left])) * (right - left);
+
+            if (key == arr[mid]) {
+                return mid;
+            } else if (key < arr[mid]) {
+                right = mid - 1; // 从左半部分查找
+            } else {
+                left = mid + 1; // 从右半部分查找
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] array = {2, 4, 8, 8, 9, 10, 13};
-        System.out.println(binarySearch(array, 4));
+        System.out.println(interpolationSearch(array, 4));
     }
 }
