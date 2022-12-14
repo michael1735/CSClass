eeefb85d5fdb355b90b98a924b71fc9f6439a314
michael1735 <25171080@qq.com>
2022-12-14 08:16:56 +0800

    弄完了, 还没测试test input

--- src/ACSL/AGRAM.java
+++ src/ACSL/AGRAM.java
@@ -1,8 +1,9 @@
 package ACSL;
 
-import javax.print.DocFlavor;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class AGRAM {
 
@@ -27,19 +28,93 @@ public class AGRAM {
         // System.out.println(opponentSuit);
         ArrayList<String> mySuit = new ArrayList<>();
         ArrayList<Integer> myRank = new ArrayList<>();
-        for (int i = 0; i < myCard.length; i++) {
-            myRank.add(toRank(myCard[i]));
-            mySuit.add(myCard[i].substring(1, 2));
+        for (String s : myCard) {
+            myRank.add(toRank(s));
+            mySuit.add(s.substring(1, 2));
         }
 
         if (mySuit.contains(opponentSuit)) {
             output = sameSuit(opponentRank, opponentSuit, myRank, mySuit);
+            // sameSuit已经可以不用管了
         } else {
-//            output = diffSuit(myRank, mySuit);
-            // @TODO 差一个diffSuit时的处理方法
+            output = diffSuit(myRank, mySuit);
+            // @TODO diffSuit完成后把这行注释取消
+//            diffSuit(myRank, mySuit);
         }
+        System.out.println(output);
     }
 
+
+    public String diffSuit(ArrayList<Integer> myRank, ArrayList<String> mySuit) {
+// @TODO 这个方法返回应该是String, 但是为了测试返回值改成了void, 记得改回来
+        ArrayList<Integer> thisRank = new ArrayList<>(myRank.size());
+        ArrayList<String> thisSuit = new ArrayList<>(mySuit.size());
+        ArrayList<Integer> listOfIndices = new ArrayList<>(myRank.size()); // store the indices where the tie occurs
+        int count = 0; // to help us determine whether there are ties
+        String minSuit = ""; // to store the suit of the minimum rank
+
+        for (int i = 0; i < myRank.size(); i++) {
+            thisRank.add(myRank.get(i));
+            thisSuit.add(mySuit.get(i));
+        }
+//        System.out.println(myRank);
+//        System.out.println(mySuit);
+//        System.out.println(thisRank);
+//        System.out.println(thisSuit);
+
+        ArrayList<Integer> tempRank = new ArrayList<>(thisRank.size());
+
+        for (int i = 0; i < thisRank.size(); i++) {
+            tempRank.add(thisRank.get(i));
+        }
+//        System.out.println(tempRank);
+//        System.out.println(tempSuit);
+        Collections.sort(tempRank);
+//        System.out.println(tempRank);
+
+        int minRank = tempRank.get(0);
+
+        for (int i = 0; i < tempRank.size(); i++) {
+            if (minRank >= thisRank.get(i)) {
+                count++;
+            }
+        }
+//        System.out.println(minRank);
+//        System.out.println(count);
+
+        minSuit = thisSuit.get(thisRank.indexOf(minRank));
+//        System.out.println(minSuit);
+        // @TODO 底下的是初步思路
+        String returnStr = "";
+        if (count == 1) {
+             return minRank + minSuit;
+            // @TODO 写完tie的解决方法(else)就可以把注释取消
+        } else {
+            for (int i = 0; i < thisRank.size(); i++) {
+                if (thisRank.get(i) == minRank) {
+                    listOfIndices.add(i);
+                }
+            }
+//            System.out.println(listOfIndices);
+            String outRank = toLetter(minRank);
+            for (int i = 0; i < listOfIndices.size(); i++) {
+                if (thisSuit.get(listOfIndices.get(i)).equals("C")) {
+                    returnStr = outRank + "C";
+                    break;
+                } else if (thisSuit.get(listOfIndices.get(i)).equals("D")) {
+                    returnStr = outRank + "D";
+                    break;
+                } else if (thisSuit.get(listOfIndices.get(i)).equals("H")) {
+                    returnStr = outRank + "H";
+                    break;
+                } else {
+                    returnStr = outRank + "S";
+                    break;
+                }
+            }
+        }
+        return returnStr;
+    }
     public String sameSuit(int oppoRank, String oppoSuit, ArrayList<Integer> myRank, ArrayList<String> mySuit) {
         String thisOppoSuit = oppoSuit; // opponentSuit when calling this func
         int thisOppoRank = oppoRank; // opponent rank
