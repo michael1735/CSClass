4fdd528b3b422632bf1bf54dabc3f6c58d79b618
michael1735 <25171080@qq.com>
2022-12-18 16:38:12 +0800

    added some comments

--- src/ACSL/NextBase.java
+++ src/ACSL/NextBase.java
@@ -10,19 +10,21 @@ public class NextBase {
         int n = 0; // how many to generate
         int b = 0; // base
         int s = 0; // where the number starts
-        String[] inArr = input.split(" ");
+        String[] inArr = input.split(" "); // split inputs
         n = Integer.parseInt(inArr[0]);
         b = Integer.parseInt(inArr[1]);
         s = Integer.parseInt(inArr[2], b);
         int num = generate(n, b, s);
+        // generate the next s numbers
         System.out.println(num);
     }
 
     static int generate(int n, int base, int s) {
-        ArrayList<String> tempList = new ArrayList<>(n);
+        ArrayList<String> tempList = new ArrayList<>(n); // store the generated number
         ArrayList<String> occurredDigit = new ArrayList<>(20);
+        // store the digits occurred inside the number;
         int start = s;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // generate the numbers and store them into tempList
             if (base == 2) {
                 tempList.add(Integer.toBinaryString(start));
             } else if (base == 10) {
@@ -37,18 +39,18 @@ public class NextBase {
             start++;
         }
 
-        for (String st : tempList) {
+        for (String st : tempList) { // find the different digit occurring
             for (int i = 0; i < st.length(); i++) {
                 if (!occurredDigit.contains(st.substring(i, i+1))) {
                     occurredDigit.add(st.substring(i, i+1));
                 }
             }
         }
-        int[] timeOccurred = new int[occurredDigit.size()];
+        int[] timeOccurred = new int[occurredDigit.size()]; // store the time each digit occurs inside the num
         String tempCh = "";
         int tempIndex = 0;
         for (String st : tempList) {
-            for (int i = 0; i < st.length(); i++) {
+            for (int i = 0; i < st.length(); i++) { // count the number of times that each digit occurs
                 tempCh = st.substring(i, i+1);
                 tempIndex = occurredDigit.indexOf(tempCh);
                 timeOccurred[tempIndex] += 1;
@@ -56,7 +58,7 @@ public class NextBase {
         }
 
         int max = 0;
-        for (int i = 0; i < timeOccurred.length; i++) {
+        for (int i = 0; i < timeOccurred.length; i++) { // find max time
             if (timeOccurred[i] > max) {
                 max = timeOccurred[i];
             }
