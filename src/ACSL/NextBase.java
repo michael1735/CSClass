d45c1fc2a7c1635c62e420473a41e54c067f6a4c
michael1735 <25171080@qq.com>
2022-12-19 20:52:48 +0800

    added a comment

--- src/ACSL/NextBase.java
+++ src/ACSL/NextBase.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class NextBase {
+    // ACSL 1st Round
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
