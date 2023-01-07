40e3893614ecbf6f94ffe54de9ee6b06613b108b
michael1735 <25171080@qq.com>
2023-01-07 12:20:32 +0800

    structure is built up

--- src/Homework/Game.java
+++ src/Homework/Game.java
@@ -1,10 +1,96 @@
 package Homework;
 
+import java.sql.SQLOutput;
 import java.util.Random;
-
+import java.util.Scanner;
+// 加入输出当前状态...
 public class Game {
-    public static void main(String[] args) {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws InterruptedException {
+        String name, race, role;
+        System.out.print("Please enter player1's name: ");
+        name = scan.nextLine();
+        System.out.print("Please enter player1's race: ");
+        race = scan.nextLine();
+        System.out.print("Please enter player1's role: ");
+        role = scan.nextLine();
+        System.out.println("Creating your character...");
+        Thread.sleep(2000);
+        Player player1 = new Player(name, race, role);
+        System.out.println("PLAYER CREATION SUCCESSFUL!!");
 
+        System.out.print("Please enter player2's name: ");
+        name = scan.nextLine();
+        System.out.print("Please enter player2's race: ");
+        race = scan.nextLine();
+        System.out.print("Please enter player2's role: ");
+        role = scan.nextLine();
+        System.out.println("Creating your character...");
+        Thread.sleep(2000);
+        Player player2 = new Player(name, race, role);
+        System.out.println("PLAYER CREATION SUCCESSFUL!!");
+
+        startPlay(player1, player2);
+        System.out.println("The game has ended...");
+        Thread.sleep(3000);
+    }
+
+    public static void startPlay(Player player1, Player player2) {
+        int action;
+        while (player1.isAlive() && player2.isAlive()) {
+            System.out.println("Now it's player1's turn!");
+            if (player1.getCurrentHP() == player1.getHP()) {
+                System.out.println("You now have the following choices: ");
+                System.out.println("1)attack");
+                System.out.print("Choose one action: ");
+                action = scan.nextInt();
+                while (action != 1) {
+                    System.out.println("You cannot choose the option! input again: ");
+                    action = scan.nextInt();
+                }
+                player2.receiveDamage(player1.attack());
+            } else {
+                System.out.println("You now have the following choices: ");
+                System.out.println("1)attack\n2)healing");
+                System.out.print("Choose one action: ");
+                action = scan.nextInt();
+                while (action != 1 && action != 2) {
+                    System.out.print("You cannot choose the option! input again: ");
+                    action = scan.nextInt();
+                }
+                if (action == 1) {
+                    player2.receiveDamage(player1.attack());
+                } else {
+                    player1.healing();
+                }
+            }
+            System.out.println("Now it's player2's turn!");
+            if (player1.getCurrentHP() == player1.getHP()) {
+                System.out.println("You now have the following choices: ");
+                System.out.println("1)attack");
+                System.out.print("Choose one action: ");
+                action = scan.nextInt();
+                while (action != 1) {
+                    System.out.println("You cannot choose the option! input again: ");
+                    action = scan.nextInt();
+                }
+                player1.receiveDamage(player2.attack());
+            } else {
+                System.out.println("You now have the following choices: ");
+                System.out.println("1)attack\n2)healing");
+                System.out.print("Choose one action: ");
+                action = scan.nextInt();
+                while (action != 1 && action != 2) {
+                    System.out.print("You cannot choose the option! input again: ");
+                    action = scan.nextInt();
+                }
+                if (action == 1) {
+                    player1.receiveDamage(player2.attack());
+                } else {
+                    player2.healing();
+                }
+            }
+        }
     }
 }
 
@@ -53,4 +139,58 @@ class Player {
         System.out.printf("当前生命: %s", currentHP);
         System.out.printf("当前法术值: %s", currentMana);
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public int getCurrentHP() {
+        return currentHP;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int attack() {
+        int amountDamaged;
+        if ((Math.random() * 100) <= criticalChance) {
+            System.out.println("CRITICAL STRIKE!");
+            amountDamaged = ATK * 2;
+        } else {
+            amountDamaged = ATK;
+        }
+        System.out.println(name + " attacked for " + amountDamaged);
+        return amountDamaged;
+    }
+
+    public void receiveDamage(int damage) {
+        HP -= damage;
+        if (currentHP <= 0) {
+            currentHP = 0;
+            alive = false;
+            System.out.println(name + " has died");
+        }
+    }
+
+    public void healing() {
+        int amountHealed = 0;
+        if ((Math.random() * 100) <= criticalChance) {
+            amountHealed += (Math.random() * intelligence) * 2;
+            System.out.println("DOUBLE HEALING!!!");
+            System.out.printf("You recovered %s", amountHealed);
+        }
+        currentMana -= 10;
+        currentHP += amountHealed;
+        if (currentHP > HP) {
+            currentHP = HP;
+        }
+        System.out.printf("%s's HP is now %d", name, currentHP);
+    }
+
+    // e技能和大以后再说
 }
---
+++ src/Homework/戚剩沼奂.java
@@ -0,0 +1,5 @@
+package Homework;
+
+public class 戚剩沼奂 {
+    // 需要regex(输入1q查看卡牌描述)和Map(选角色)
+}
