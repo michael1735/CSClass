d1771880021e275bec6587236a9126ca9847f328
michael1735 <25171080@qq.com>
2023-01-13 09:12:50 +0800

    decimalFormat

--- src/JavaPackage/DecimalFormatTest.java
+++ src/JavaPackage/DecimalFormatTest.java
@@ -1,5 +1,14 @@
 package JavaPackage;
 
-public class DecimalFormatTest {
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
+public class DecimalFormatTest {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("###.00"); // ¿´pattern matching
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        double d = 3.145;
+        System.out.println(df.format(d));
+    }
+    // +BigDecimal
 }
