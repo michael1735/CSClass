083dac1bc2b000b6809a80da53ad572b43ffe175
michael1735 <25171080@qq.com>
2023-01-20 17:00:18 +0800

    annotationDemo.java

---
+++ src/JavaPackage/AnnotationDemo.java
@@ -0,0 +1,16 @@
+package JavaPackage;
+
+public class AnnotationDemo {
+    // Annotations, a form of metadata, provide data about a program that is not part of the program itself.
+    // Annotations have no direct effect on the operation of the code they annotate.
+    /*
+    simplest form : @Entity
+
+    include elements :
+    @Author(
+        name = "Benjamin Franklin",
+        date = "3/27/2003"
+    )
+    class MyClass{...}
+     */
+}
