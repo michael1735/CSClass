216a65b273c67d06411e46023e4bb3a87e6b360d
michael1735 <25171080@qq.com>
2023-01-20 16:45:38 +0800

    enum practise

--- src/JavaPackage/EnumTest/CardTest.java
+++ src/JavaPackage/EnumTest/CardTest.java
@@ -3,7 +3,7 @@ package JavaPackage.EnumTest;
 import java.util.Arrays;
 
 public class CardTest {
-    private static Card[] cards = new Card[52];
+    private static final Card[] cards = new Card[52];
 
     public static void main(String[] args) {
         int i = 0;
@@ -13,5 +13,13 @@ public class CardTest {
             }
         }
         System.out.println(Arrays.toString(cards));
+        cards[0] = new Card(Rank.ACE, Suit.HEART);
+        System.out.println(Arrays.toString(cards));
+
+        /*
+        对于一个final变量:
+        如果是基本类型, 则其数值在初始化后就不能更改;
+        如果是引用类型, 对其初始化后便不能再让其指向另一个对象
+         */
     }
 }
