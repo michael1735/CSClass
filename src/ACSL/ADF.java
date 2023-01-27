7de8bb0939a8d5c2be849f6c1f83313194144b94
michael1735 <25171080@qq.com>
2023-01-28 02:04:41 +0800

    first ver. completed

--- src/ACSL/ADF.java
+++ src/ACSL/ADF.java
@@ -1,13 +1,44 @@
 package ACSL;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ADF {
     // ACSL difference factor
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        String str1 = scan.nextLine();
-        String str2 = scan.nextLine();
+        String[] in1 = {"I am going home now", "I will go home now"};
+        String[] in2 = {"The big black bear bit a big black bug", "The big black bug bled black blood"};
+        String[] in3 = {"Complementary angle measures sum to 90 degrees.", "The measures of supplementary angles add to 180 degrees."};
+        String[] in4 = {"A Tale of Two Cities was published by Dickens in 1859.", "In 1839, Charles Dickens published Nicholas Nickleby."};
+        String[] in5 = {"Connecticut is The Constitution State.", "Hartford is the capital of Connecticut."};
+
+        System.out.println(diffFactor(in1[0], in1[1]));
+        System.out.println(diffFactor(in2[0], in2[1]));
+        System.out.println(diffFactor(in3[0], in3[1]));
+        System.out.println(diffFactor(in4[0], in4[1]));
+        System.out.println(diffFactor(in5[0], in5[1]));
+    }
+
+    public static int diffFactor(String str1, String str2) {
+        ArrayList<String> longestCommonSubStr = new ArrayList<>();
+        String string1 = "";
+        String string2 = "";
+        for (char c : str1.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                string1 = string1.concat(String.valueOf(c));
+            }
+        }
+
+        for (char c : str2.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                string2 = string2.concat(String.valueOf(c));
+            }
+        }
+        string1 = string1.toUpperCase();
+        string2 = string2.toUpperCase();
+
         
+        return 0;
     }
 }
--- src/ACSL/ForceFindPrimeFactors.java
+++ src/ACSL/ForceFindPrimeFactors.java
@@ -1,24 +1,30 @@
 package ACSL;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ForceFindPrimeFactors {
-    // 强行分解质因数的方法
+    // 仍然是强制找质因数, 但是只会输出一个质数一次
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
         int k = 2;
-        System.out.print(m + " = ");
+        int m = sc.nextInt();
+        ArrayList<Integer> existingFactors = new ArrayList<Integer>();
         while (k <= m) {
             if (k == m) {
                 System.out.println(m);
+                if (!existingFactors.contains(m))
+                    existingFactors.add(m); // 防止出现16807这种7的倍数或者更广义的说, 次方
+                // 次方情况下如果不加这个判断就会加入两个相同的质因数进去
                 break;
             } else if (m % k == 0) {
-                System.out.print(k + " * ");
-                m = m / k;
+                System.out.println(k + " * ");
+                m = m / k; // 这一行放下一行上面的原因是如果放在下面的if里的话就会导致不断重复, 因为m没有更新
+                if (!existingFactors.contains(k)) // 如果不存在我就加到记录器里
+                    existingFactors.add(k);
             } else {
-                k++; // k慢慢的变大
+                k++;
             }
         }
+        System.out.println(existingFactors);
     }
 }
